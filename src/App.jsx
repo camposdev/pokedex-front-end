37ed8f6ed6653b3a180efbb01ead8fe094c4e738
Felipe Campos <felipecamposdev@gmail.com>
2022-08-10 19:42:50 -0300

    remove helmet library

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Helmet } from 'react-helmet'
 import { Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import styled, { ThemeProvider } from 'styled-components'
@@ -19,16 +18,6 @@ const App = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Helmet>
-        <meta name="theme-color" content={theme.palette.red} />
-        <title>Pokedex</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,300;0,400;0,700;1,400&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
       <GlobalStyles />
 
       <Header />
